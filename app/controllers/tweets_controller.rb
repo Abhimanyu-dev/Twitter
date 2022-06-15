876f53f257ea0a555d7e8622fc781c014bbd3d88
Abhimanyu-dev <abhimanyusolanki4841@gmail.com>
2022-06-16 02:15:22 +0530

    Finished Project

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!, except: [:index, :show]
   # GET /tweets or /tweets.json
   def index
     @tweets = Tweet.all.order("created_at DESC")
@@ -13,7 +13,7 @@ class TweetsController < ApplicationController
 
   # GET /tweets/new
   def new
-    @tweet = Tweet.new
+    @tweet = current_user.tweets.build
   end
 
   # GET /tweets/1/edit
@@ -22,12 +22,12 @@ class TweetsController < ApplicationController
 
   # POST /tweets or /tweets.json
   def create
-    @tweet = Tweet.new(tweet_params)
+    @tweet = current_user.tweets.build(tweet_params)
 
     respond_to do |format|
       if @tweet.save
         format.html { redirect_to root_path, notice: "Tweet was successfully created." }
-        format.json { render :show, status: :created, location: @tweet }
+        format.json { render root_path, status: :created, location: @tweet }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @tweet.errors, status: :unprocessable_entity }
@@ -39,8 +39,8 @@ class TweetsController < ApplicationController
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
-        format.html { redirect_to tweet_url(@tweet), notice: "Tweet was successfully updated." }
-        format.json { render :show, status: :ok, location: @tweet }
+        format.html { redirect_to root_path, notice: "Tweet was successfully updated." }
+        format.json { render root_path, status: :ok, location: @tweet }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @tweet.errors, status: :unprocessable_entity }
